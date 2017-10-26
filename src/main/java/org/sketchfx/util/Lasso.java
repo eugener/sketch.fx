3807b932b6ed6ccb1093ec3d048af77726531fc6
Eugene Ryzhikov <eryzhikov@gmail.com>
2017-10-26 12:10:13 -0400

    Show size hint suring lasso move

--- src/main/java/org/sketchfx/util/Lasso.java
+++ src/main/java/org/sketchfx/util/Lasso.java
@@ -5,6 +5,7 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.geometry.Point2D;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 
@@ -16,6 +17,7 @@ public class Lasso extends AbstractDragSupport {
 
     private Rectangle rect;
     private Pane lassoPane;
+    private Label sizeHint;
 
     public Lasso(Pane clickPane, Pane lassoPane ) {
         super(clickPane);
@@ -37,9 +39,11 @@ public class Lasso extends AbstractDragSupport {
     @Override
     protected void dragBegin(Point2D dragStart) {
         Point2D start = lassoPane.sceneToLocal(dragStart);
+        sizeHint = new Label();
+        sizeHint.getStyleClass().add("canvas-hint");
         rect = new Rectangle( start.getX(), start.getY(), 0, 0);
         rect.setStyle("-fx-fill: transparent; -fx-stroke: deepskyblue;");
-        lassoPane.getChildren().add(rect);
+        lassoPane.getChildren().addAll(rect, sizeHint);
     }
 
     @Override
@@ -47,13 +51,18 @@ public class Lasso extends AbstractDragSupport {
 
         rect.setWidth(rect.getWidth() + deltas.getX() );
         rect.setHeight(rect.getHeight() + deltas.getY() );
+        sizeHint.setLayoutX( rect.getX() + rect.getWidth() + 10 );
+        sizeHint.setLayoutY( rect.getY() + rect.getHeight() + 10 );
+        sizeHint.setText( rect.getWidth() + " x " + rect.getHeight() );
+
     }
 
     @Override
     protected void dragEnd() {
-        lassoPane.getChildren().remove(rect);
+        lassoPane.getChildren().removeAll(rect, sizeHint);
         if ( getOnFinished() != null ) getOnFinished().accept(rect);
         rect = null;
+        sizeHint = null;
     }
 
     public void suspend() {
